d4b98090f046772712f4eb85a4a4455cae796eba
NorbiVis <norbi_vis@yahoo.com>
2021-02-27 12:11:02 +0200

    project

--- hacker_news/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import News, Comment,Account, Like, Hide, ReplayComment
-
-admin.site.register(News)
-admin.site.register(Comment)
-admin.site.register(Account)
-admin.site.register(Like)
-admin.site.register(Hide)
-admin.site.register(ReplayComment)
