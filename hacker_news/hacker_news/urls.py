35084ae66127f12311a679049699c925b0f7656a
NorbiVis <norbi_vis@yahoo.com>
2021-02-27 11:33:40 +0200

    heroku changes

--- hacker_news/hacker_news/urls.py
+++ hacker_news/hacker_news/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
     path('accounts/', include('allauth.urls')),
-    path('social-auth/', include('social_django.urls', namespace="social"))
-]
+    path('social-auth/', include('social_django.urls', namespace="social")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
